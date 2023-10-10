758ebe0c41bcb6fc5da67e757f786b577a4a8607
Marcin <marcinegzmont@gmail.com>
2023-10-10 11:34:06 +0100

    17 event binding

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -6,7 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./product-list.component.css'],
 })
 export class ProductListComponent {
-  name = 'John Doe';
+  // name = 'John Doe';
+  addToCart: number = 0;
   product = {
     name: 'iPhone',
     price: 1000,
@@ -19,5 +20,14 @@ export class ProductListComponent {
     return (
       this.product.price - (this.product.price * this.product.discount) / 100
     );
+  }
+  onNameChange(event: any) {
+    // this.name = event.target.value;
+  }
+  add() {
+    if (this.addToCart < this.product.inStock) this.addToCart++;
+  }
+  subtract() {
+    if (this.addToCart > 0) this.addToCart--;
   }
 }
